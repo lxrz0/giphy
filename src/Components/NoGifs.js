85216ed624d4d0412ada783ccceb55539276044f
christianal <christianlorza@googlemail.com>
2017-09-14 23:09:33 +0000

    First Commit

---
+++ src/Components/NoGifs.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoGifs = props => {
+    return(
+        <li className="no-gifs">
+            <h3>Sorry, no GIFs match your search</h3>
+        </li>
+    );
+};
+
+export default NoGifs;
